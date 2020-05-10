6467a736a1ea6ba2124ba4783d8fe47c61d825c6
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-05-10 17:59:04 -0400

    process/resource: Completely refactor
    Move everything into an enum and add more OS-specific limits

--- src/process/resource.rs
+++ src/process/resource.rs
@@ -6,35 +6,66 @@ use super::super::error;
 
 // Work around GNU not implementing the POSIX standard correctly
 #[cfg(any(target_env = "", target_env = "gnu"))]
-pub type Resource = libc::__rlimit_resource_t;
+type RawResourceType = libc::__rlimit_resource_t;
 
 #[cfg(not(any(target_env = "", target_env = "gnu")))]
-pub type Resource = i32;
+type RawResourceType = i32;
 
-pub const RLIMIT_AS: Resource = libc::RLIMIT_AS;
-pub const RLIMIT_CORE: Resource = libc::RLIMIT_CORE;
-pub const RLIMIT_CPU: Resource = libc::RLIMIT_CPU;
-pub const RLIMIT_DATA: Resource = libc::RLIMIT_DATA;
-pub const RLIMIT_NOFILE: Resource = libc::RLIMIT_NOFILE;
-pub const RLIMIT_FSIZE: Resource = libc::RLIMIT_FSIZE;
-pub const RLIMIT_STACK: Resource = libc::RLIMIT_STACK;
 
-#[cfg(target_os = "linux")]
-pub const RLIMIT_MEMLOCK: Resource = libc::RLIMIT_MEMLOCK;
-#[cfg(target_os = "linux")]
-pub const RLIMIT_MSGQUEUE: Resource = libc::RLIMIT_MSGQUEUE;
-#[cfg(target_os = "linux")]
-pub const RLIMIT_NICE: Resource = libc::RLIMIT_NICE;
-#[cfg(target_os = "linux")]
-pub const RLIMIT_NPROC: Resource = libc::RLIMIT_NPROC;
-#[cfg(target_os = "linux")]
-pub const RLIMIT_RSS: Resource = libc::RLIMIT_RSS;
-#[cfg(target_os = "linux")]
-pub const RLIMIT_RTPRIO: Resource = libc::RLIMIT_RTPRIO;
-#[cfg(target_os = "linux")]
-pub const RLIMIT_RTTIME: Resource = libc::RLIMIT_RTTIME;
-#[cfg(target_os = "linux")]
-pub const RLIMIT_SIGPENDING: Resource = libc::RLIMIT_SIGPENDING;
+#[repr(i32)]
+pub enum Resource {
+    // OpenBSD is missing this for some reason
+    #[cfg(not(target_os = "openbsd"))]
+    AS = libc::RLIMIT_AS as i32,
+
+    // Should be present on all POSIX systems
+    CORE = libc::RLIMIT_CORE as i32,
+    CPU = libc::RLIMIT_CPU as i32,
+    DATA = libc::RLIMIT_DATA as i32,
+    NOFILE = libc::RLIMIT_NOFILE as i32,
+    FSIZE = libc::RLIMIT_FSIZE as i32,
+    STACK = libc::RLIMIT_STACK as i32,
+
+    // Linux and the BSDs
+    #[cfg(any(target_os = "linux", target_os = "openbsd", target_os = "freebsd", target_os = "netbsd", target_os = "dragonfly"))]
+    NPROC = libc::RLIMIT_NPROC as i32,
+    #[cfg(any(target_os = "linux", target_os = "openbsd", target_os = "freebsd", target_os = "netbsd", target_os = "dragonfly"))]
+    MEMLOCK = libc::RLIMIT_MEMLOCK as i32,
+    #[cfg(any(target_os = "linux", target_os = "openbsd", target_os = "freebsd", target_os = "netbsd", target_os = "dragonfly"))]
+    RSS = libc::RLIMIT_RSS as i32,
+
+    // Most of the BSDs (but not OpenBSD)
+    #[cfg(any(target_os = "freebsd", target_os = "netbsd", target_os = "dragonfly"))]
+    SBSIZE = libc::RLIMIT_SBSIZE as i32,
+
+    // FreeBSD-specific
+    #[cfg(target_os = "freebsd")]
+    KQUEUES = libc::RLIMIT_KQUEUES as i32,
+    #[cfg(target_os = "freebsd")]
+    SWAP = libc::RLIMIT_SWAP as i32,
+    #[cfg(target_os = "freebsd")]
+    NPTS = libc::RLIMIT_NPTS as i32,
+
+    // NetBSD-specific
+    #[cfg(target_os = "netbsd")]
+    NTHR = libc::RLIMIT_NTHR as i32,
+
+    // DragonFly BSD-specific
+    #[cfg(target_os = "dragonfly")]
+    POSIXLOCKS = libc::RLIMIT_POSIXLOCKS as i32,
+
+    // Linux-specific
+    #[cfg(target_os = "linux")]
+    MSGQUEUE = libc::RLIMIT_MSGQUEUE as i32,
+    #[cfg(target_os = "linux")]
+    NICE = libc::RLIMIT_NICE as i32,
+    #[cfg(target_os = "linux")]
+    RTPRIO = libc::RLIMIT_RTPRIO as i32,
+    #[cfg(target_os = "linux")]
+    RTTIME = libc::RLIMIT_RTTIME as i32,
+    #[cfg(target_os = "linux")]
+    SIGPENDING = libc::RLIMIT_SIGPENDING as i32,
+}
 
 
 pub type Limit = libc::rlim_t;
@@ -45,7 +76,7 @@ pub fn getrlimit(resource: Resource) -> io::Result<(Limit, Limit)> {
     let mut rlim = libc::rlimit { rlim_cur: LIMIT_INFINITY, rlim_max: LIMIT_INFINITY };
 
     error::convert_nzero(unsafe {
-        libc::getrlimit(resource, &mut rlim);
+        libc::getrlimit(resource as RawResourceType, &mut rlim);
     }, rlim).map(|rlim| (rlim.rlim_cur, rlim.rlim_max))
 }
 
@@ -53,7 +84,7 @@ pub fn setrlimit(resource: Resource, new_limits: (Limit, Limit)) -> io::Result<(
     let rlim = libc::rlimit { rlim_cur: new_limits.0, rlim_max: new_limits.1 };
 
     error::convert_nzero(unsafe {
-        libc::setrlimit(resource, &rlim);
+        libc::setrlimit(resource as RawResourceType, &rlim);
     }, ())
 }
 
@@ -71,7 +102,7 @@ pub fn prlimit(pid: i32, resource: Resource, new_limits: Option<(Limit, Limit)>)
     let mut old_rlim = libc::rlimit { rlim_cur: LIMIT_INFINITY, rlim_max: LIMIT_INFINITY };
 
     error::convert_nzero(unsafe {
-        libc::prlimit(pid, resource, new_rlim_ptr, &mut old_rlim);
+        libc::prlimit(pid, resource as RawResourceType, new_rlim_ptr, &mut old_rlim);
     }, old_rlim).map(|old_rlim| (old_rlim.rlim_cur, old_rlim.rlim_max))
 }
 
