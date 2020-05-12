46e9e901a2b74e7c23a999aa84a2bce4d06527bc
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-05-12 17:31:25 -0400

    net: Significant refactor

--- src/net.rs
+++ src/net.rs
@@ -2,33 +2,30 @@ use std::io;
 use std::os::unix;
 use std::os::unix::io::AsRawFd;
 
+use super::{GidT, Int, UidT};
+
 cfg_if::cfg_if! {
     if #[cfg(any(target_os = "linux", target_os = "openbsd"))] {
         // Linux and OpenBSD use almost exactly the same interface.
         // The only difference is the order of the fields in the
         // credentials struct, and we can special-case that.
 
-        #[cfg(target_os = "linux")]
+        /// Stores the received credentials. WARNING: Several aspects of this struct
+        /// are system-dependent!
         #[derive(Debug)]
         #[repr(C)]
         pub struct Ucred {
-            pub pid: i32,
-            pub uid: u32,
-            pub gid: u32,
+            #[cfg(target_os = "linux")]
+            pub pid: super::PidT,
+
+            pub uid: UidT,
+            pub gid: GidT,
+
+            #[cfg(not(target_os = "linux"))]
+            pub pid: super::PidT,
         }
 
-        #[cfg(not(target_os = "linux"))]
-        #[derive(Debug)]
-        #[repr(C)]
-        pub struct Ucred {
-            pub uid: u32,
-            pub gid: u32,
-            pub pid: i32,
-        }
-
-        /// Reads credentials for a `SOCK_STREAM` socket. Behavior when given a
-        /// `SOCK_DGRAM` socket is unspecified and system-dependent.
-        pub fn get_ucred_raw(sockfd: i32) -> io::Result<Ucred> {
+        fn get_ucred_raw_impl(sockfd: Int) -> io::Result<Ucred> {
             let mut ucred = Ucred {
                 pid: 0,
                 uid: 0,
@@ -50,10 +47,20 @@ cfg_if::cfg_if! {
     }
 }
 
-/// Attempts to read credentials from the given Unix socket.
+/// Attempts to read credentials from the given Unix stream socket.
 ///
 /// Only supported on specific platforms.
 #[cfg(any(target_os = "linux", target_os = "openbsd"))]
 pub fn get_ucred(sock: &unix::net::UnixStream) -> io::Result<Ucred> {
     get_ucred_raw(sock.as_raw_fd())
+}
+
+/// Reads credentials for a `SOCK_STREAM` socket. Behavior when given a
+/// `SOCK_DGRAM` socket is unspecified and system-dependent.
+///
+/// Only supported on specific platforms.
+#[cfg(any(target_os = "linux", target_os = "openbsd"))]
+#[inline(always)]
+pub fn get_ucred_raw(sockfd: Int) -> io::Result<Ucred> {
+    get_ucred_raw_impl(sockfd)
 }
