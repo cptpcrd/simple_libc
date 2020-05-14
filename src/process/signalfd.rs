9e9111a226f410e924ad70cadb670534eac00bb1
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-05-13 21:02:34 -0400

    process/signalfd: Use custom Siginfo struct directly
    Avoids copying data around. This required some tweaks to the struct
    itself.

--- src/process/signalfd.rs
+++ src/process/signalfd.rs
@@ -17,45 +17,29 @@ impl SignalFd {
     }
 
     pub fn read_one(&self) -> io::Result<Siginfo> {
-        let mut siginfo: libc::signalfd_siginfo = unsafe { std::mem::zeroed() };
+        let mut siginfo: Siginfo = unsafe { std::mem::zeroed() };
 
         error::convert_neg(
             unsafe {
                 libc::read(
                     self.fd,
-                    (&mut siginfo as *mut libc::signalfd_siginfo) as *mut libc::c_void,
-                    std::mem::size_of::<libc::signalfd_siginfo>(),
+                    (&mut siginfo as *mut Siginfo) as *mut libc::c_void,
+                    std::mem::size_of::<Siginfo>(),
                 )
             },
-            &siginfo,
+            siginfo,
         )
-        .map(Siginfo::from)
     }
 
     pub fn read(&self, siginfos: &mut [Siginfo]) -> io::Result<usize> {
-        let length = siginfos.len();
-        let size = std::mem::size_of::<libc::signalfd_siginfo>();
-
-        let mut raw_siginfos: Vec<libc::signalfd_siginfo> = Vec::new();
-        raw_siginfos.reserve(length);
-        unsafe {
-            raw_siginfos.set_len(length);
-        }
-
         error::convert_neg_ret(unsafe {
             libc::read(
                 self.fd,
-                raw_siginfos.as_mut_ptr() as *mut libc::c_void,
-                size * length,
+                siginfos.as_mut_ptr() as *mut libc::c_void,
+                siginfos.len() * std::mem::size_of::<Siginfo>(),
             )
         })
-        .and_then(|n| {
-            let n = (n as usize) / size;
-            for (i, raw_siginfo) in raw_siginfos.iter().take(n).enumerate() {
-                siginfos[i] = Siginfo::from(raw_siginfo);
-            }
-            Ok(n)
-        })
+        .map(|n| (n as usize) / std::mem::size_of::<Siginfo>())
     }
 }
 
@@ -73,9 +57,10 @@ impl Drop for SignalFd {
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Default, Debug, Copy, Clone)]
+#[repr(C)]
 pub struct Siginfo {
-    pub sig: Int,
+    pub sig: u32,
     pub errno: i32,
     pub code: i32,
     pub pid: u32,
@@ -92,54 +77,7 @@ pub struct Siginfo {
     pub stime: u64,
     pub addr: u64,
     pub addr_lsb: u16,
-}
-
-impl From<&libc::signalfd_siginfo> for Siginfo {
-    fn from(siginfo: &libc::signalfd_siginfo) -> Siginfo {
-        Siginfo {
-            sig: siginfo.ssi_signo as Int,
-            errno: siginfo.ssi_errno,
-            code: siginfo.ssi_code,
-            pid: siginfo.ssi_pid,
-            uid: siginfo.ssi_uid,
-            fd: siginfo.ssi_fd,
-            tid: siginfo.ssi_tid,
-            band: siginfo.ssi_band,
-            overrun: siginfo.ssi_overrun,
-            trapno: siginfo.ssi_trapno,
-            status: siginfo.ssi_status,
-            int: siginfo.ssi_int,
-            ptr: siginfo.ssi_ptr,
-            utime: siginfo.ssi_utime,
-            stime: siginfo.ssi_stime,
-            addr: siginfo.ssi_addr,
-            addr_lsb: siginfo.ssi_addr_lsb,
-        }
-    }
-}
-
-impl From<Siginfo> for libc::signalfd_siginfo {
-    fn from(siginfo: Siginfo) -> libc::signalfd_siginfo {
-        let mut sinfo: libc::signalfd_siginfo = unsafe { std::mem::zeroed() };
-
-        sinfo.ssi_signo = siginfo.sig as u32;
-        sinfo.ssi_errno = siginfo.errno;
-        sinfo.ssi_code = siginfo.code;
-        sinfo.ssi_pid = siginfo.pid;
-        sinfo.ssi_uid = siginfo.uid;
-        sinfo.ssi_fd = siginfo.fd;
-        sinfo.ssi_tid = siginfo.tid;
-        sinfo.ssi_band = siginfo.band;
-        sinfo.ssi_overrun = siginfo.overrun;
-        sinfo.ssi_trapno = siginfo.trapno;
-        sinfo.ssi_status = siginfo.status;
-        sinfo.ssi_int = siginfo.int;
-        sinfo.ssi_ptr = siginfo.ptr;
-        sinfo.ssi_utime = siginfo.utime;
-        sinfo.ssi_stime = siginfo.stime;
-        sinfo.ssi_addr = siginfo.addr;
-        sinfo.ssi_addr_lsb = siginfo.addr_lsb;
-
-        sinfo
-    }
+    // WARNING: This is dependent on the size of the fields above!
+    _padding: [u8; 32],
+    _padding2: [u8; 14],
 }
