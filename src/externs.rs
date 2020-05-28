202410951e3d84ec4d3bd8c3f90798199f24d9d2
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-05-28 16:48:07 -0400

    externs: Refactor to avoid use of cfg_if!

--- src/externs.rs
+++ src/externs.rs
@@ -1,5 +1,3 @@
-use cfg_if::cfg_if;
-
 // The libc crate is missing some functions. <sigh>
 // Some of these just aren't there period; some aren't present for
 // certain platforms that definitely have them.
@@ -25,14 +23,24 @@ extern "C" {
     ) -> libc::c_int;
 }
 
-cfg_if! {
-    if #[cfg(any(target_os = "linux", target_os = "freebsd", target_os = "openbsd", target_os = "dragonfly"))] {
-        extern "C" {
-            pub fn getresuid(ruid: *mut libc::uid_t, euid: *mut libc::uid_t, suid: *mut libc::uid_t) -> libc::c_int;
-            pub fn setresuid(ruid: libc::uid_t, euid: libc::uid_t, suid: libc::uid_t) -> libc::c_int;
+#[cfg(any(
+    target_os = "linux",
+    target_os = "freebsd",
+    target_os = "openbsd",
+    target_os = "dragonfly",
+))]
+extern "C" {
+    pub fn getresuid(
+        ruid: *mut libc::uid_t,
+        euid: *mut libc::uid_t,
+        suid: *mut libc::uid_t,
+    ) -> libc::c_int;
+    pub fn setresuid(ruid: libc::uid_t, euid: libc::uid_t, suid: libc::uid_t) -> libc::c_int;
 
-            pub fn getresgid(rgid: *mut libc::gid_t, egid: *mut libc::gid_t, sgid: *mut libc::gid_t) -> libc::c_int;
-            pub fn setresgid(rgid: libc::gid_t, egid: libc::gid_t, sgid: libc::gid_t) -> libc::c_int;
-        }
-    }
+    pub fn getresgid(
+        rgid: *mut libc::gid_t,
+        egid: *mut libc::gid_t,
+        sgid: *mut libc::gid_t,
+    ) -> libc::c_int;
+    pub fn setresgid(rgid: libc::gid_t, egid: libc::gid_t, sgid: libc::gid_t) -> libc::c_int;
 }
