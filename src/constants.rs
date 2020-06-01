10658117eeeb7da8cba3dfa6225bfbddfa67b2a8
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-05-31 23:11:58 -0400

    constants: Add underscores in file capability-related numeric literals

--- src/constants.rs
+++ src/constants.rs
@@ -66,13 +66,13 @@ crate::attr_group! {
     pub const _LINUX_CAPABILITY_VERSION_3: u32 = 0x2008_0522;
 
     // File capabilities constants
-    pub const VFS_CAP_FLAGS_EFFECTIVE: u32 = 0x000001;
+    pub const VFS_CAP_FLAGS_EFFECTIVE: u32 = 0x00_0001;
 
-    pub const VFS_CAP_REVISION_1: u32 = 0x01000000;
+    pub const VFS_CAP_REVISION_1: u32 = 0x0100_0000;
     pub const XATTR_CAPS_SZ_1: usize = 12;
-    pub const VFS_CAP_REVISION_2: u32 = 0x02000000;
+    pub const VFS_CAP_REVISION_2: u32 = 0x0200_0000;
     pub const XATTR_CAPS_SZ_2: usize = 20;
-    pub const VFS_CAP_REVISION_3: u32 = 0x03000000;
+    pub const VFS_CAP_REVISION_3: u32 = 0x0300_0000;
     pub const XATTR_CAPS_SZ_3: usize = 24;
 
     pub const XATTR_NAME_CAPS: &str = "security.capability";
