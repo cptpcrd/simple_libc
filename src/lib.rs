a5735a3967ccaccf331ba5f64fde7d35fbbb3ff7
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-06-25 11:58:47 -0400

    lib: Add *listxattr() wrappers

--- src/lib.rs
+++ src/lib.rs
@@ -626,6 +626,110 @@ attr_group! {
             buf.resize(buf.len() * 2, 0);
         }
     }
+
+    pub fn listxattr_raw(
+        path: &ffi::CStr,
+        list: &mut [u8],
+        follow_links: bool,
+    ) -> io::Result<usize> {
+        let callback_fn = if follow_links {
+            libc::listxattr
+        } else {
+            libc::llistxattr
+        };
+
+        let n = error::convert_neg_ret(unsafe {
+            callback_fn(
+                path.as_ptr(),
+                list.as_mut_ptr() as *mut Char,
+                list.len(),
+            )
+        })?;
+
+        Ok(n as usize)
+    }
+
+    pub fn listxattr<P: AsRef<ffi::OsStr>>(
+        path: P,
+        follow_links: bool,
+    ) -> io::Result<Vec<ffi::OsString>> {
+        let c_path = ffi::CString::new(path.as_ref().as_bytes())?;
+
+        let mut c_list = Vec::new();
+        let init_size = listxattr_raw(&c_path, &mut c_list, follow_links)?;
+        c_list.resize(init_size, 0);
+
+        loop {
+            match listxattr_raw(&c_path, &mut c_list, follow_links) {
+                Ok(n) => {
+                    c_list.resize(n as usize, 0);
+                    break;
+                }
+                Err(e) => {
+                    if !error::is_erange(&e) || c_list.len() > init_size * 4 {
+                        return Err(e);
+                    }
+                }
+            }
+
+            c_list.resize(c_list.len() * 2, 0);
+        }
+
+        let mut res = Vec::new();
+
+        let mut it = c_list.into_iter().peekable();
+        while it.peek().is_some() {
+            res.push(ffi::OsString::from_vec(it.by_ref().take_while(|x| *x != 0).collect()));
+        }
+
+        Ok(res)
+    }
+
+    pub fn flistxattr_raw(
+        fd: Int,
+        list: &mut [u8],
+    ) -> io::Result<usize> {
+        let n = error::convert_neg_ret(unsafe {
+            libc::flistxattr(
+                fd,
+                list.as_mut_ptr() as *mut Char,
+                list.len(),
+            )
+        })?;
+
+        Ok(n as usize)
+    }
+
+    pub fn flistxattr(fd: Int) -> io::Result<Vec<ffi::OsString>> {
+        let mut c_list = Vec::new();
+        let init_size = flistxattr_raw(fd, &mut c_list)?;
+        c_list.resize(init_size, 0);
+
+        loop {
+            match flistxattr_raw(fd, &mut c_list) {
+                Ok(n) => {
+                    c_list.resize(n as usize, 0);
+                    break;
+                }
+                Err(e) => {
+                    if !error::is_erange(&e) || c_list.len() > init_size * 4 {
+                        return Err(e);
+                    }
+                }
+            }
+
+            c_list.resize(c_list.len() * 2, 0);
+        }
+
+        let mut res = Vec::new();
+
+        let mut it = c_list.into_iter().peekable();
+        while it.peek().is_some() {
+            res.push(ffi::OsString::from_vec(it.by_ref().take_while(|x| *x != 0).collect()));
+        }
+
+        Ok(res)
+    }
 }
 
 #[cfg(test)]
@@ -809,5 +913,17 @@ mod tests {
     #[test]
     fn test_domainname() {
         getdomainname().unwrap();
+    }
+
+    #[cfg(target_os = "linux")]
+    #[test]
+    fn test_listxattr() {
+        let current_exe = std::env::current_exe().unwrap();
+
+        listxattr(&current_exe, false).unwrap();
+        listxattr(&current_exe, true).unwrap();
+
+        let f = fs::File::open(&current_exe).unwrap();
+        flistxattr(f.as_raw_fd()).unwrap();
     }
 }
