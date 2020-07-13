c2c05b53bcc05929ad4900b9d89a956f3b56eda6
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-07-12 21:18:59 -0400

    rusage: Fix for platforms with extra fields in rusage struct

--- src/rusage.rs
+++ src/rusage.rs
@@ -61,30 +61,7 @@ impl From<&libc::rusage> for Rusage {
 }
 
 fn get_raw(target: Target) -> libc::rusage {
-    let mut rusage = libc::rusage {
-        ru_utime: libc::timeval {
-            tv_sec: 0,
-            tv_usec: 0,
-        },
-        ru_stime: libc::timeval {
-            tv_sec: 0,
-            tv_usec: 0,
-        },
-        ru_maxrss: 0,
-        ru_ixrss: 0,
-        ru_idrss: 0,
-        ru_isrss: 0,
-        ru_minflt: 0,
-        ru_majflt: 0,
-        ru_nswap: 0,
-        ru_inblock: 0,
-        ru_oublock: 0,
-        ru_msgsnd: 0,
-        ru_msgrcv: 0,
-        ru_nsignals: 0,
-        ru_nvcsw: 0,
-        ru_nivcsw: 0,
-    };
+    let mut rusage: libc::rusage = unsafe { std::mem::zeroed() };
 
     let retval = unsafe { libc::getrusage(target as Int, &mut rusage) };
 
